143d471d5624d8179c3a865f791c34da7b3fc756
cbolk <cristiana.bolchini@polimi.it>
2025-10-02 13:47:03 +0200

    Lesson 1
    material

--- codice/03.timeinsec.1.py
+++ codice/03.timeinsec.1.py
@@ -6,11 +6,4 @@ SEC_IN_HR = (SEC_IN_MIN * MIN_IN_HR)
 # DATA ACQUISITION
 timesec = int(input())
 
-# COMPUTATION
-num_hr = timesec // SEC_IN_HR
-timerem = timesec % SEC_IN_HR
-num_min = timerem // SEC_IN_MIN
-num_sec = timerem % SEC_IN_MIN
-
-# VISUALISATION
-print(num_hr, num_min, num_sec)
+# 
